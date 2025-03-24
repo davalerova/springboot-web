78e111aa7b36dd3639aba4bc2f0c36335d4f4758
David Valero <dvdovni@gmail.com>
2025-03-24 10:46:17 -0500

    Add list endpoint to UserRestController to return a list of users

--- src/main/java/com/daval/curso/sprigboot/webapp/springboot_web/controllers/UserRestController.java
+++ src/main/java/com/daval/curso/sprigboot/webapp/springboot_web/controllers/UserRestController.java
@@ -1,6 +1,8 @@
 package com.daval.curso.sprigboot.webapp.springboot_web.controllers;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import org.springframework.web.bind.annotation.RestController;
@@ -28,6 +30,20 @@ public class UserRestController {
         userDto.setTitle("Hola mundo spring boot");
         userDto.setUser(user);
         return userDto;
+    }
+
+    @GetMapping("/list")
+    public List<User> list() {
+        User user = new User("David", "Valero");
+        User user2 = new User("Andrés", "Vanegas");
+        User user3 = new User("Jhon", "Doe");
+        // 1) List<User> users = List.of(user, user2, user3);
+        // 2) List<User> users = Arrays.asList(user, user2, user3);
+        List<User> users = new ArrayList<>();
+        users.add(user);
+        users.add(user2);
+        users.add(user3);
+        return users;
     }
 
     @GetMapping("/details-map")
