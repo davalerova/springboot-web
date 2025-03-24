00d19537749a296e57f803457693023214f5cf36
David Valero <dvdovni@gmail.com>
2025-03-24 10:23:46 -0500

    Add UserDto class and update UserRestController to return UserDto

--- src/main/java/com/daval/curso/sprigboot/webapp/springboot_web/controllers/UserRestController.java
+++ src/main/java/com/daval/curso/sprigboot/webapp/springboot_web/controllers/UserRestController.java
@@ -6,6 +6,7 @@ import java.util.Map;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.daval.curso.sprigboot.webapp.springboot_web.models.User;
+import com.daval.curso.sprigboot.webapp.springboot_web.models.UserDto;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,7 +22,16 @@ public class UserRestController {
     }
     
     @GetMapping("/details")
-    public Map<String, Object> details() {
+    public UserDto details() {
+        User user = new User("David", "Valero");
+        UserDto userDto = new UserDto();
+        userDto.setTitle("Hola mundo spring boot");
+        userDto.setUser(user);
+        return userDto;
+    }
+
+    @GetMapping("/details-map")
+    public Map<String, Object> detailsMap() {
         User user = new User("David", "Valero");
         Map<String, Object> body = new HashMap<>() ;
         body.put("title", "Hola mundo spring boot");
---
+++ src/main/java/com/daval/curso/sprigboot/webapp/springboot_web/models/UserDto.java
@@ -0,0 +1,21 @@
+package com.daval.curso.sprigboot.webapp.springboot_web.models;
+
+public class UserDto {
+    private String title;
+    private User user;
+    
+    public String getTitle() {
+        return title;
+    }
+    public void setTitle(String title) {
+        this.title = title;
+    }
+    public User getUser() {
+        return user;
+    }
+    public void setUser(User user) {
+        this.user = user;
+    }
+    
+
+}
